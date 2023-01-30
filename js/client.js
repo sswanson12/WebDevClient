660aa581f3bfec4b709fea17b39daa7991509b49
sswanson12 <sswanson12@my.wctc.edu>
2023-01-30 10:59:07 -0600

    Toast complete with data values and can be exited with the escape key.

---
+++ js/client.js
@@ -0,0 +1,34 @@
+﻿$(function(){
+    var toast = new Audio('media/toast.wav');
+
+    $('.code').on('click', function(e) {
+        e.preventDefault();
+
+        // first pause the audio (in case it is still playing)
+        toast.pause();
+
+        // reset the audio
+        toast.currentTime = 0;
+
+        // play audio
+        toast.play();
+
+        let code = $(e.currentTarget).data('discountcode')
+        let productName = $(e.currentTarget).data('product')
+        let discount = $(e.currentTarget).data('discount')
+
+        $('#discountPercent').html(discount);
+
+        $('#code').html(code);
+
+        $('#product').html(productName);
+
+        $('#toast').toast({ autohide: false }).toast('show');
+    });
+
+    document.onkeydown = function(e) {
+        if (e.key === 'Escape'){
+            $('#toast').toast('hide');
+        }
+    };
+});
